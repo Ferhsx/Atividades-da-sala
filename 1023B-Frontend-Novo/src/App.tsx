9fbbc36c8f20c755a74b03d7aff73bbdec231102
ferculus <165941104+Ferhsx@users.noreply.github.com>
2025-09-15 08:32:46 -0400

    commite3

--- 1023B-Frontend-Novo/src/App.tsx
+++ 1023B-Frontend-Novo/src/App.tsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+interface ProdutoType {
+  id: number
+  name: string
+  price: number
+  urlfoto: string
+  description: string
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [produtos, setProdutos] = useState<ProdutoType[]>([])
+
+  useEffect(() => {
+    fetch('/api/produtos')
+      .then(response => response.json())
+      .then(data => setProdutos(data))
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 123123)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Produtos</h1>
+      <ul style={{ display: 'flex', flexDirection: 'row', gap: '10px' }}>
+        {produtos.map(produto => (
+          <li key={produto.id} style={{ margin: '10px' }}>
+            <img src={produto.urlfoto} alt={produto.name} width={200} />
+            <p>{produto.description}</p>
+            <p>R$ {produto.price}</p>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
